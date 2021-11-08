00ac975c33bd36c35f8edd5267be3d3b595a3729
Michael Uti <utimichael9@gmail.com>
2021-11-08 09:48:09 +0000

    ci: update cargo crates

--- tools/scripts/release/fetch-crates.sh
+++ tools/scripts/release/fetch-crates.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-while read line
-do
-    (
-        cd "implementations/rust/ockam/$line";
-        rm -rf release.toml
-    )
-done < <(ls "implementations/rust/ockam")
+val=$(eval "cargo metadata --no-deps | jq '[.packages[] | {name: .name, version: .version, release: .metadata.release.release}]'")
+length=$(eval "echo '$val' | jq '. | length' ")
+echo "$length"
+length=$(eval "echo '$val' | jq '. | length' ")
+
+for (( c=0; c<$length; c++ ))
+  do 
+    repo_name=$(eval "echo '$val' | jq '.[$c].name' | tr -d '\"' ")
+    release=$(eval "echo '$val' | jq '.[$c].release' ")
+    version=$(eval "echo '$val' | jq '.[$c].version' | tr -d '\"' ")
+    if $release == true; then
+        (
+            cd ~/vendor;
+            echo "https://static.crates.io/crates/${repo_name}/${repo_name}-${version}.crate"
+            if curl "https://static.crates.io/crates/${repo_name}/${repo_name}-${version}.crate" --output "${repo_name}-${version}.crate"; then
+                echo "Downloaded $repo_name"
+            else
+                echo "Error downloading $repo_name"
+            fi
+        )
+    fi
+done
