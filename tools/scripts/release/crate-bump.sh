62c7e256e1c0d369b167fceeea4d12a8fbbac44e
Andrew Grangaard <granny-github@ofb.net>
2022-10-30 23:56:57 -0700

    ci: fix easy shellcheck warnings

--- tools/scripts/release/crate-bump.sh
+++ tools/scripts/release/crate-bump.sh
@@ -24,7 +24,7 @@ declare -A specified_crate_version
 
 crate_array=($OCKAM_BUMP_MODIFIED_RELEASE)
 
-for word in ${crate_array[@]}; do
+for word in "${crate_array[@]}"; do
   key="${word%%:*}"
   value="${word##*:}"
   specified_crate_version[$key]=$value
@@ -62,9 +62,9 @@ bumping_transitive_deps=false
 # on the second iteration, (crateC and crateB) version is then bumped, on the third iteration
 # we do not bump crateC version even though its dep has been modified as it's version has already
 # been bumped for a release.
-while [[ $updated_crates != $recently_updated_crates ]]; do
-  for crate in ${updated_crates[@]}; do
-    if [[ ! -z "${bumped_crates[$crate]}" ]]; then
+while [[ "$updated_crates" != "$recently_updated_crates" ]]; do
+  for crate in "${updated_crates[@]}"; do
+    if [[ -n "${bumped_crates[$crate]}" ]]; then
       echo "===> $crate has been bumped recently ignoring"
       continue
     fi
@@ -78,7 +78,7 @@ while [[ $updated_crates != $recently_updated_crates ]]; do
     name=$(eval "tomlq package.name -f implementations/rust/ockam/$crate/Cargo.toml")
 
     # Check if crate version was specified manually
-    if [[ ! -z "${specified_crate_version[$crate]}" ]]; then
+    if [[ -n "${specified_crate_version[$crate]}" ]]; then
       echo "Bumping $crate version specified manually as ${specified_crate_version[$crate]}"
       version="${specified_crate_version[$crate]}"
     fi
@@ -93,7 +93,7 @@ while [[ $updated_crates != $recently_updated_crates ]]; do
   bumping_transitive_deps=true
 
   source tools/scripts/release/crates-to-publish.sh
-  echo "Recently bumped crates are $recently_updated_crates \n updated crates are $updated_crates"
+  printf "Recently bumped crates are %s \n updated crates are %s" "$recently_updated_crates" "$updated_crates"
 done
 
 echo "Bumped crates $recently_updated_crates"
